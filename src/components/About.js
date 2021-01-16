f618f0a09d4922f864f6a1edfdefab03d3421324
Ganesh Kale <kaleg2025@gmail.com>
2021-01-16 19:21:18 +0530

    Components  Added

--- src/components/About.js
+++ src/components/About.js
@@ -1,41 +1,53 @@
 import React from "react";
 import logo2 from "../images/banner/secondimage.png";
 
-
 const About = () => {
-    return (
-            <section id="about" className="about-area">
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-lg-6 col-md-12 let-image">
-              <div className="about-image">
-                <img src={logo2} alt="About US IMAGE" className="img-fluid animate__animated animate__backInLeft" />
-              </div>
-            </div>
-            <div className="col-lg-6 col-md-12 about-two">
-              <h2 className="text-uppercase pt-5">
-                <span>Let me</span>
-                <span>introdunce</span>
-                <span>Myself</span>
-              </h2>
-              <div className="paragraph py-4 w-75">
-                <p className="para">
-                  Hii, I’m A Full Stack Web Developer, Competitive Coder & I Like To Design Beautiful Responsive 
-                  Websites & Solving Complex Algorithm Problems. I Love to Learn & Explorer Varous New Technologies..
-                </p>
-                <h2><span className="text-uppercase sech2">Skills</span></h2>
-                <p className="para">
-                  Web: MERN, ReactJS, NodeJS, Redux, Python Django<br/>
-                  Database: MySQL, Firebase, MongoDB<br/>
-                  Competitive Coder: On HackerRank, CodeForces, CodeChef, HackerEarth<br/>
-                  Languages: C, C++, Java, JavaScript, Python
-                </p>
-              </div>
-              <button type="button" className="btn btn-primary text-uppercase">DownLoad CV</button>
+  return (
+    <section id="about" className="about-area">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-6 col-md-12 let-image">
+            <div className="about-image">
+              <img
+                src={logo2}
+                className="img-fluid animate__animated animate__backInLeft"
+              />
             </div>
           </div>
+          <div className="col-lg-6 col-md-12 about-two">
+            <h2 className="text-uppercase pt-5">
+              <span>Let me</span>
+              <span>introdunce</span>
+              <span>Myself</span>
+            </h2>
+            <div className="paragraph py-4 w-75">
+              <p className="para">
+                Hii, I’m A Full Stack Web Developer, Competitive Coder & I Like
+                To Design Beautiful Responsive Websites & Solving Complex
+                Algorithm Problems. I Love to Learn & Explorer Varous New
+                Technologies..
+              </p>
+              <h2>
+                <span className="text-uppercase sech2">Skills</span>
+              </h2>
+              <p className="para">
+                Web: MERN, ReactJS, NodeJS, Redux, Python Django
+                <br />
+                Database: MySQL, Firebase, MongoDB
+                <br />
+                Competitive Coder: On HackerRank, CodeForces, CodeChef,
+                HackerEarth
+                <br />
+                Languages: C, C++, Java, JavaScript, Python
+              </p>
+            </div>
+            <button type="button" className="btn btn-primary text-uppercase">
+              DownLoad CV
+            </button>
+          </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 export default About;
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,51 +1,89 @@
 import React from "react";
 import cont from "../images/banner/question.png";
 
-
 const Contact = () => {
-    return (
-        <section id="contact" className="contactsection">
-        <div className="container-fluid">
-            <div className="row">
-                <div className="col-lg-8 form">
-                    <form className="sentMessage" action="https://formspree.io/f/mayllryq" method="POST" id="contactForm" novalidate>
-                        <div className="row">
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <input type="text" name="fullname" className="form-control" placeholder="Your Name *" id="name" required data-validation-required-message="Please enter your name." />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                                <div className="form-group">
-                                    <input type="email" name="email" className="form-control" placeholder="Your Email *" id="email" required data-validation-required-message="Please enter your email address." />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                                <div className="form-group">
-                                    <input type="tel" name="phone" className="form-control" placeholder="Your Phone *" id="phone" required data-validation-required-message="Please enter your phone number." />
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div className="col-md-6">
-                                <div className="form-group">
-                                    <textarea name="text-area" className="form-control" placeholder="Your Message *" id="message" required data-validation-required-message="Please enter a message."></textarea>
-                                    <p className="help-block text-danger"></p>
-                                </div>
-                            </div>
-                            <div className="clearfix"></div>
-                            <div className="col-lg-12 text-center contactbutton">
-                                <div id="success"></div>
-                                <button name="submit" type="submit" className="btn btn-xl">Send Message</button>
-                            </div>
-                        </div>
-                    </form>
+  return (
+    <section id="contact" className="contactsection">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-lg-8 form">
+            <form
+              className="sentMessage"
+              action="https://formspree.io/f/mayllryq"
+              method="POST"
+              id="contactForm"
+              novalidate
+            >
+              <div className="row">
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <input
+                      type="text"
+                      name="fullname"
+                      className="form-control"
+                      placeholder="Your Name *"
+                      id="name"
+                      required
+                      data-validation-required-message="Please enter your name."
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="email"
+                      name="email"
+                      className="form-control"
+                      placeholder="Your Email *"
+                      id="email"
+                      required
+                      data-validation-required-message="Please enter your email address."
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
+                  <div className="form-group">
+                    <input
+                      type="tel"
+                      name="phone"
+                      className="form-control"
+                      placeholder="Your Phone *"
+                      id="phone"
+                      required
+                      data-validation-required-message="Please enter your phone number."
+                    />
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
-                <div className="col-lg-4">
-                  <div className="col-lg-12 img">
-                    <img src={cont} alt="thinking image" />
+                <div className="col-md-6">
+                  <div className="form-group">
+                    <textarea
+                      name="text-area"
+                      className="form-control"
+                      placeholder="Your Message *"
+                      id="message"
+                      required
+                      data-validation-required-message="Please enter a message."
+                    ></textarea>
+                    <p className="help-block text-danger"></p>
+                  </div>
                 </div>
+                <div className="clearfix"></div>
+                <div className="col-lg-12 text-center contactbutton">
+                  <div id="success"></div>
+                  <button name="submit" type="submit" className="btn btn-xl">
+                    Send Message
+                  </button>
                 </div>
+              </div>
+            </form>
+          </div>
+          <div className="col-lg-4">
+            <div className="col-lg-12 img">
+              <img src={cont} />
             </div>
+          </div>
         </div>
+      </div>
     </section>
-    );
+  );
 };
 export default Contact;
